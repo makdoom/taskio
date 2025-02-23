b03ba876497f001291a0d790e4718dcc20e7ba04
Makdoom Shaikh <makshaikh99@gmail.com>
2025-02-23 14:44:32 +0530

    feat(current-user): Add logout & fetch current user feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <h1>Home</h1>;
-}
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useCurrentUser } from "@/features/auth/api/user-current-user";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const Home = () => {
+  const router = useRouter();
+  const { data, isLoading } = useCurrentUser();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
+  if (isLoading) return <p>loading ...</p>;
+  return (
+    <h1>
+      Home-authorized user
+      <Button onClick={() => mutate()}>Logout</Button>
+    </h1>
+  );
+};
+
+export default Home;
